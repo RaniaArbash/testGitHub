8b52b90e23e150ee0497af08113206a96d8a9f07
Rania <raniaarbash@hotmail.com>
2019-05-31 10:19:06 -0400

    add collection view protocols functions

--- testGitHub/ViewController.swift
+++ testGitHub/ViewController.swift
@@ -8,7 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UICollectionViewDataSource,UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
